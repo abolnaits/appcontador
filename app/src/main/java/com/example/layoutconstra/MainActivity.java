6f2fb98c72f6cf3eda3637d4e2eafa41fd38ac3f
Andres Benitez <abolnaits@hotmail.com>
2021-01-18 19:44:37 -0500

    Base

---
+++ app/src/main/java/com/example/layoutconstra/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.layoutconstra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    //Variables
+    public int contador;
+    //Vistas
+    TextView txtNumero;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Init
+        txtNumero = (TextView) findViewById(R.id.txtNumero);
+        contador = 0;
+    }
+
+    //Setear el valor del textp
+    /*
+    public void setear(){
+        TextView txtContador = (TextView) findViewById(R.id.txtContador);
+        txtContador.setText(String.valueOf(contador));
+    }
+    */
+    public void sumar(View Vista){
+        contador = contador + 1;
+        txtNumero.setText(String.valueOf(contador));
+        //setear();
+    }
+
+    public void restar(View Vista){
+        contador = contador - 1;
+        if(contador<0){
+            //Obtengo el checkbox
+            CheckBox ckNegativo = (CheckBox) findViewById(R.id.ckNegativo);
+            if(ckNegativo.isChecked()){
+                //txtNumero.setText(String.valueOf(contador));
+            }else{
+                contador = 0;
+                //txtNumero.setText(String.valueOf(contador));
+            }
+        }
+        txtNumero.setText(String.valueOf(contador));
+
+        //setear();
+    }
+
+    public void reset(View Vista){
+        contador = 0;
+        //setear();
+    }
+}
